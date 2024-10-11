1db5ae19bd1eff3be286ee2dcfaa3ad6f8152d87
arletino <titkov@nxt.ru>
2024-10-11 21:23:53 +0300

    done action yaml cl.py

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import uvicorn
 
 
 app = FastAPI()
@@ -8,4 +9,4 @@ async def root():
     return {'message': "hello World"}
 
 if __name__ == '__main__':
-    
+    uvicorn.run('main:app', host="0.0.0.0", port=8080, reload=True)
