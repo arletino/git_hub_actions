2b80b44a88b52d31b3b69270eb94ce85d8f1f800
arletino <titkov@nxt.ru>
2024-10-11 21:53:25 +0300

    try install poetry in action

--- test/test_01.py
+++ test/test_01.py
@@ -4,7 +4,7 @@ import requests
 def test_root():
     req = requests.get('http://localhost:8080')
     print(req.text)
-    assert req.text == {"message":"hello World"}
+    assert req.text == '{"message":"hello World"}'
 
 if __name__ == '__main__':
     # pytest.main()
